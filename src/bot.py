cb927d58a4586964d090c94b29707da703c6abf2
marvelman3284 <jedimaster3284@gmail.com>
2022-07-11 21:35:46 -0400

    add better help command

--- src/bot.py
+++ src/bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import yaml, discord, sys, logging
 
 from cogs.utils import Utils
+from help import ChadHelp
 
 class Chad(commands.Bot):
     """An extension of discord.ext.commands.Bot with configuration
@@ -10,7 +11,7 @@ class Chad(commands.Bot):
     def __init__(self,
                  command_prefix,
                  configpath: str,
-                 help_command=commands.DefaultHelpCommand(),
+                 help_command=ChadHelp(),
                  description=None,
                  **options):
         self.configpath = configpath
@@ -24,16 +25,12 @@ class Chad(commands.Bot):
     def getConfig(self, section: str, item: str) -> str:
         """Returns a String value from the bot's config file"""
         if item == 'token':
-            # Yes, I'm aware that the original ConfigParser is avalible.
-            # This is more of a reminder for me
             raise PermissionError('You can\'t get the token during runtime')
         return self.config[section][item]
 
     def setConfig(self, section: str, item: str, value: str) -> None:
         """Changes a configuration value, and writes it to the file"""
         if section == "API":
-            # Yes, I'm aware that the original ConfigParser is avalible.
-            # This is more of a reminder for me
             raise PermissionError(
                 "You can't modify API settings while the bot is running!")
         self.config[section][item] = value
